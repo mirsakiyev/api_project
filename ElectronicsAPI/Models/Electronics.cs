4a6d07b7fbb7da71b9c1eaf3177ce295ff2b07cd
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-12-05 19:22:55 -0500

    Ran Scaffold-DbContext

---
+++ ElectronicsAPI/Models/Electronics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElectronicsAPI.Models
+{
+    public partial class Electronics
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ElectronicsAPI/Models/electronicsdbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ElectronicsAPI.Models
+{
+    public partial class electronicsdbContext : DbContext
+    {
+        public electronicsdbContext()
+        {
+        }
+
+        public electronicsdbContext(DbContextOptions<electronicsdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Electronics> Electronics { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=electronicsdb.cezkcufzbnsc.us-east-2.rds.amazonaws.com,1433; database=electronicsdb;User ID=aslanmr4project; Password=Xmj01k401;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Electronics>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Category)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manufacturer)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(6, 2)");
+            });
+        }
+    }
+}
